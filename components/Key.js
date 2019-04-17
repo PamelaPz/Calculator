0fdfb9927136446dd1b60dbd7557b043128b9052
JessicaPamela <jpzcode@gmail.com>
2019-04-17 11:24:13 -0500

    Code more clear

---
+++ components/Key.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+export default class KEy extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <TouchableOpacity
+                  style={this.props.styles}
+                  onPress={this.props.action}>
+                <Text style={this.props.stylesText}> {this.props.text} </Text>
+            </TouchableOpacity>
+        )
+    }
+}
